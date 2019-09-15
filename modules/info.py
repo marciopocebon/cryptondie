0188df050f0e720380f6f42c45c6933dcde3f469
zer0dx <gmdutra.root@gmail.com>
2019-09-15 15:03:35 -0300

    Add banner and loginfo functions

---
+++ modules/info.py
@@ -0,0 +1,36 @@
+from termcolor import colored
+from time import sleep
+
+def banner():
+    print(colored('''
+                /(        )`
+                \ \___   / |
+                /- _  `-/  '
+               (/\/ \ \   /\\
+               / /   | `    \\
+               O O   ) /    |
+               `-^--'`<     '
+              (_.)  _  )   /
+               `.___/`    /
+                 `-----' /
+    <----.     __ / __   \\
+    <----|====O)))==) \) /====
+    <----'    `--' `.__,' \\
+                 |        |
+                  \       /
+             ______( (_  / \______
+           ,'  ,-----'   |        \\
+           `--{__________)        \/
+
+Author: zer0dx
+Github: github.com/zer0dx
+Blog:   https://zer0dx.github.io
+''', "yellow"))
+
+    sleep(3)
+
+def log(msg):
+    info = colored("[+]INFO: ", "yellow")
+    msg = colored(msg, "green")
+
+    print("{0}{1}".format(info, msg))
